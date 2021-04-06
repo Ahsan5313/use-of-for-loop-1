b4ac065bf8783647a277be8b5d9f67026a1b74de
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-06 17:20:37 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,21 @@
+
+for(var x=1;x<=5;x=x+1){
+    var num=parseFloat(prompt("Enter your first number :"));
+    var num1=parseFloat(prompt("Enter your last number :"));
+    number=num+num1;
+    document.write(" Result =" +number +"<br/>");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
